814be571a44eb272a7b7d11817995b0d765a3365
Keith Valin <keith@mailtux.net>
2025-02-02 23:13:40 -0500

    Impelmented JSON parsing of needed objects

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::fs::File;
+
+use chrono::NaiveDate;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+struct DiscordNotifications {
+    message: String,
+    webhook_url: String,
+    since: NaiveDate,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::open("example.json");
+    if file.is_err() {
+        panic!("Error opening example.json!");
+    }
+    let data: Vec<DiscordNotifications> = serde_json::from_reader(file.unwrap()).expect("Not valid json");
+    for item in data {
+        println!("{}", item.message);
+    }
 }
